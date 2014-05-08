59646d50bcb2db801567feb1405b8e76a2080716
Faissal M. Sleiman <faissalsleiman@gmail.com>
2014-05-08 08:13:11 -0400

    renamed sphinx directories

--- ext/sphinxbase/include/sphinx_config.h
+++ ext/sphinxbase/include/sphinx_config.h
@@ -41,7 +41,7 @@
 /* #undef FIXED_POINT */
 
 /* The size of `long', as computed by sizeof. */
-#define SIZEOF_LONG 4
+#define SIZEOF_LONG 8
 
 /* Define to 1 if the system has the type `long long'. */
 #define HAVE_LONG_LONG 1
