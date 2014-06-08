88175b2b6ef04b15b087f4d021bc2769cf2f9486
hadiesper <hadiesper@gmail.com>
2014-06-09 02:45:05 +0400

    Improved Face Recognition
    Added functions for Face Deviation from center and face distance from
    camera.

--- ext/OpenCVFaceRecognition/Main.cpp
+++ ext/OpenCVFaceRecognition/Main.cpp
@@ -7,24 +7,21 @@
  
 using namespace std;
 using namespace cv;
+
+int CAPTUREWIDTH = 640; //in pixels
+int CAPTUREHEIGHT = 480; //in pixels
  
 double getFaceDistance(int faceWidth, int faceHeight)
 {
-	double invertedarea = 1.0 - ((faceWidth*faceHeight) / (double)(640 * 480));
-	if (invertedarea == 0.5)
-	{
-		return 1.0;
-	}
-	if (invertedarea < 0.5)
-	{
-		return 1.0 - invertedarea;
-	}
-	if (invertedarea > 0.5)
-	{
-		return 1.0 + invertedarea;
-	}
-	return -1;
+	return ((double)CAPTUREWIDTH/ faceWidth) * (double)18;
+
+	//FORMULA: f = d*Z / D
+	//d = faceWidth in Pixels
+	//Z = Distance to Face
+	//D = Actual Face Width
+	//f = Capture Width in Pixels
 }
+
 int main(int argc, const char** argv)
 {
     //create the cascade classifier object used for the face detection
@@ -34,7 +31,11 @@ int main(int argc, const char** argv)
  
     //setup video capture device and link it to the first capture device
     VideoCapture captureDevice;
+	
     captureDevice.open(0);
+	//Set Dimensions
+	captureDevice.set(CV_CAP_PROP_FRAME_HEIGHT, CAPTUREHEIGHT);
+	captureDevice.set(CV_CAP_PROP_FRAME_WIDTH, CAPTUREWIDTH);
  
     //setup image files used in the capture process
     Mat captureFrame;
@@ -66,20 +67,22 @@ int main(int argc, const char** argv)
             Point pt1(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
             Point pt2(faces[i].x, faces[i].y);
 
-			
             rectangle(captureFrame, pt1, pt2, cvScalar(0, 255, 0, 0), 1, 8, 0);
 			cout<<"FACE DETECTED: " << faces.size() <<endl;
-			cout << "FACE Coordinates: " << faces[i].x << "," << faces[i].y << endl;
-			cout<<"FACE DIMENTIONS: " <<faces[i].width << "x" << faces[i].height <<endl;
-			cout << "Distance in Meters: " << getFaceDistance(faces[i].width,faces[i].height) << endl;
+			cout << "Face Size in Pixels: " << faces[i].width << "x" << faces[i].height << endl;
+			cout << "Center of FACE Coordinates: " << faces[i].x + (faces[i].width / 2) << "," << faces[i].y + (faces[i].height / 2) << endl;
+			cout << "Shift from Center of Camera: " << faces[i].x + (faces[i].width / 2) - (CAPTUREWIDTH / 2) << "," << faces[i].y + (faces[i].height / 2) - (CAPTUREWIDTH/2) << endl; //assuming 640x480 res. so center of the image is at 320,240. 
+			cout << "Approx. Distance in cm: " << getFaceDistance(faces[i].width,faces[i].height) << endl;
         }
  
         //print the output
         imshow("outputCapture", captureFrame);
  
-        //pause for 33ms
-        waitKey(330);
+        //pause for X-ms
+        waitKey(100);
     }
  
     return 0;
 }
+
+
